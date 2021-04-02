f1c2b364c4671038d0bd4442f47feed19630ca00
ranie <ranie.black29@gmail.com>
2021-04-02 11:36:25 -0300

    atualizacao

--- quiz/base/views.py
+++ quiz/base/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render, redirect
 from quiz.base.forms import AlunoForm
-from quiz.base.models import Pergunta
+from quiz.base.models import Pergunta, Aluno
 
 
 def home(requisicao):
     if requisicao.method == 'POST':
-        formulario = AlunoForm(requisicao.POST)
-        if formulario.is_valid():
-            aluno = formulario.save()
-            return redirect('/perguntas/1')
+        # Usuario ja Existe
+        email = requisicao.POST['email']
+        try:
+            aluno = Aluno.objects.get(email=email)
+        except Aluno.DoesNotExist:
+            # Usuario nao Existe
+            formulario = AlunoForm(requisicao.POST)
+            if formulario.is_valid():
+                aluno = formulario.save()
+                requisicao.session['aluno_id'] = aluno.id
+                return redirect('/perguntas/1')
+            else:
+                contexto = {'formulario': formulario}
+                return render(requisicao, 'base/home.html', contexto)
+        else:
+            requisicao.session['aluno_id'] = aluno.id
     return render(requisicao, 'base/home.html')
 
 
@@ -17,7 +29,22 @@ def classificacao(requisicao):
 
 
 def perguntas(requisicao, indice):
-    pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
-    contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
-    return render(requisicao, 'base/game.html', context=contexto)
+    try:
+        aluno_id = requisicao.session['aluno_id']
+    except KeyError:
+        return redirect('/')
+    else:
+        try:
+            pergunta = Pergunta.objects.filter(disponivel=True).order_by('id')[indice - 1]
+        except IndexError:
+            return redirect('/classificacao')
+        else:
+            contexto = {'indice_da_questao': indice, 'pergunta': pergunta}
+            if requisicao.method == 'POST':
+                resposta_indice = int(requisicao.POST['resposta_indice'])
+                if resposta_indice == pergunta.alternativas_correta:
+                    # Amazenar dados da resposta
+                    return redirect(f'perguntas/{indice + 1}')
+                contexto['resposta_indice'] = resposta_indice
+            return render(requisicao, 'base/game.html', context=contexto)
 
